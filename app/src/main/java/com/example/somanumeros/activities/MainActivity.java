32a1c8ee01e1f4bc758cd2273142e323d6996445
José Guilherme <jose.guilherme.pax@gmail.com>
2020-08-12 21:53:44 -0300

    V1; Implementação da MainActivity

--- app/src/main/java/com/example/somanumeros/activities/MainActivity.java
+++ app/src/main/java/com/example/somanumeros/activities/MainActivity.java
@@ -3,14 +3,61 @@ package com.example.somanumeros.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.somanumeros.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText n1EditText;
+    private EditText n2EditText;
+
+    private Button somarButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        n1EditText = findViewById(R.id.edittext_n1);
+        n2EditText = findViewById(R.id.edittext_n2);
+        somarButton = findViewById(R.id.button_somar);
+
+        somarButton.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == somarButton){
+            somar();
+        }
+    }
+
+    private void somar(){
+        double n1, n2, soma;
+
+        try{
+            n1 = Double.valueOf(n1EditText.getText().toString());
+            n2 = Double.valueOf(n2EditText.getText().toString());
+
+        } catch (NumberFormatException ex){
+            n1 = 0;
+            n2 = 0;
+
+            Toast.makeText(this, "Erro na entrada de dados", Toast.LENGTH_SHORT).show();
+
+            return;
+        }
+
+        soma = n1 + n2;
+
+        Toast.makeText(this, String.format("%.2f", soma), Toast.LENGTH_SHORT).show();
+
+
     }
 }
