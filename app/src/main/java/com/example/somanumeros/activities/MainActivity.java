e87ec96c21c39f7686869d18e3ce948f7ba3e610
José Guilherme <jose.guilherme.pax@gmail.com>
2020-08-12 22:04:28 -0300

    V2: Ajuste na MainActivity

--- app/src/main/java/com/example/somanumeros/activities/MainActivity.java
+++ app/src/main/java/com/example/somanumeros/activities/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private EditText n1EditText;
     private EditText n2EditText;
+    private EditText n3EditText;
 
     private Button somarButton;
 
@@ -25,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         n1EditText = findViewById(R.id.edittext_n1);
         n2EditText = findViewById(R.id.edittext_n2);
+        n3EditText = findViewById(R.id.edittext_n3);
         somarButton = findViewById(R.id.button_somar);
 
         somarButton.setOnClickListener(this);
@@ -39,22 +41,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void somar(){
-        double n1, n2, soma;
+        double n1, n2, n3, soma;
 
         try{
             n1 = Double.valueOf(n1EditText.getText().toString());
             n2 = Double.valueOf(n2EditText.getText().toString());
+            n3 = Double.valueOf(n3EditText.getText().toString());
 
         } catch (NumberFormatException ex){
             n1 = 0;
             n2 = 0;
+            n3 = 0;
 
             Toast.makeText(this, "Erro na entrada de dados", Toast.LENGTH_SHORT).show();
 
             return;
         }
 
-        soma = n1 + n2;
+        soma = n1 + n2 + n3;
 
         Toast.makeText(this, String.format("Soma: %.2f", soma), Toast.LENGTH_SHORT).show();
 
